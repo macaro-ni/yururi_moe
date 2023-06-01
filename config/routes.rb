2b9d9b1a9daecd6e1392ac49a0cb1068338b26dd
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-06-01 14:57:57 +0000

    [add]activestorage,devise(admin,customer)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions',
+  passwords: 'public/passwords'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
   get root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
