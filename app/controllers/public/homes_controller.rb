c0e4e5fc0f1f14044d971e83c8409faab5a849d0
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-06-22 02:24:37 +0000

    [add]public_page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
+    @pictures=Picture.limit(4)
   end
 end
--- app/controllers/public/pictures_controller.rb
+++ app/controllers/public/pictures_controller.rb
@@ -1,7 +1,9 @@
 class Public::PicturesController < ApplicationController
   def index
+    @pictures=Picture.all
   end
 
   def show
+    @picture=Picture.find(params[:id])
   end
 end
